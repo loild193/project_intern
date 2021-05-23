f687ef553338ad35612557222cfce846f29b5e29
LeDucLoi193 <dekaito193@gmail.com>
2021-05-23 23:23:19 +0700

    add create reaquest ui

---
+++ client/src/customStyles/CreateRequestStyles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core";
+
+export const createRequestStyle = makeStyles({
+	decription: {
+		width: "100%",
+	},
+	formControl: {
+		minWidth: 200,
+	},
+}); 
